3913520dd9dfd6650c260a7728a14182a7f98579
Theo <theomalidiorientation@gmail.com>
2024-01-25 11:23:47 +0100

    save ray

--- src/visuals/window.c
+++ src/visuals/window.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 13:28:46 by tmalidi           #+#    #+#             */
-/*   Updated: 2024/01/24 14:11:42 by tmalidi          ###   ########.fr       */
+/*   Updated: 2024/01/25 11:20:20 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,45 @@ int	ft_key(int key, t_data *gd)
 	return (0);
 }
 
+void draw_line(t_data *gd, int x1, int y1, int x2, int y2, int color)
+{
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
+
+    while (1)
+    {
+        mlx_pixel_put(gd->mlx, gd->win, x1, y1, color);
+
+        if (x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+        if (e2 > -dy)
+        {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx)
+        {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
 void ft_put_windows(t_data *game_data)
 {
+    //int i = 0;
+    //int y = 0;
+    //int j = 0;
+    //int x = 0;
     game_data->mlx = mlx_init();
     game_data->win = mlx_new_window(game_data->mlx,720,480,"Cube3D");
+    draw_line(game_data, 10, 10, 200, 100, 0xFFFFFF);
+    draw_line(game_data, 10, 10, 200, 200, 0xFFFFFF);
     mlx_key_hook(game_data->win, ft_key, game_data);
     mlx_hook(game_data->win, 17, 1l << 0, exit_game, game_data);
     mlx_loop(game_data->mlx);
