612c64bebc00b5a9f3bb1ecf0d49983df5555d89
Theo Malidi <tmalidi@z1r11p5.42lyon.fr>
2024-03-20 18:07:43 +0100

    save ok

--- src/visuals/texture.c
+++ src/visuals/texture.c
@@ -6,13 +6,13 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:43:07 by tmalidi           #+#    #+#             */
-/*   Updated: 2024/03/20 17:58:36 by tmalidi          ###   ########.fr       */
+/*   Updated: 2024/03/20 18:07:34 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void init_tex_data(t_tex_data *data, t_data *game_data, int ray)
+void	init_tex_data(t_tex_data *data, t_data *game_data, int ray)
 {
 	data->bpp = 0;
 	data->sl = 0;
@@ -26,14 +26,14 @@ void init_tex_data(t_tex_data *data, t_data *game_data, int ray)
 	data->original_x = ray % 72;
 }
 
-char *new_display(t_data *game_data, char *wall_data, int ray)
+char	*new_display(t_data *game_data, char *wall_data, int ray)
 {
 	t_tex_data	data;
-	int 		y;
+	int			y;
 	int			i;
-	
+
 	y = 0;
-	init_tex_data(&data,game_data,ray);
+	init_tex_data(&data, game_data, ray);
 	while (y < 1080)
 	{
 		data.original_y = y * data.original_height / 1080;
@@ -43,9 +43,11 @@ char *new_display(t_data *game_data, char *wall_data, int ray)
 		i = 0;
 		while (i < data.bpp / 8)
 		{
-			if (data.original_index + i < data.original_width * data.original_height
-				* (data.bpp / 8) && data.enlarged_index + i < 1 * 1080 * (data.bpp / 8))
-				data.enlarged_wall_data[data.enlarged_index + i] = wall_data[data.original_index + i];
+			if (data.original_index + i < data.original_width
+				* data.original_height * (data.bpp / 8)
+				&& data.enlarged_index + i < 1 * 1080 * (data.bpp / 8))
+				data.enlarged_wall_data[data.enlarged_index + i]
+					= wall_data[data.original_index + i];
 			i++;
 		}
 		y++;
