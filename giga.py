012c9a7dc7a1f1963f091f444484f12208db1908
Jocelyn Renault <jrenault@z3r5p6.42lyon.fr>
2024-04-16 11:13:39 +0200

    improved giga.py

--- giga.py
+++ giga.py
@@ -5,7 +5,7 @@ height = 500
 
 file = open("gigatesteur.cub", "w")
 
-file .write("NO textures/wall_north.xpm\nSO textures/wall_south.xpm\nWE textures/wall_west.xpm\nEA textures/wall_east.xpm\n\nF 255,255,255\nC 0,255,0\n\n")
+file .write("NO textures/wall_north.xpm\nSO textures/wall_south.xpm\nWE textures/wall_west.xpm\nEA textures/wall_east.xpm\n\nF 119,207,238\nC 0,255,0\n\n")
 
 i = 0
 n = False
