45040d2c53182253a908ae742fb5debad69eca87
Theo <theomalidiorientation@gmail.com>
2024-04-16 08:34:22 +0200

    minimap for debug

--- src/visuals/key.c
+++ src/visuals/key.c
@@ -6,7 +6,7 @@
 /*   By: tmalidi <tmalidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 18:54:28 by tmalidi           #+#    #+#             */
-/*   Updated: 2024/04/03 18:35:42 by tmalidi          ###   ########.fr       */
+/*   Updated: 2024/04/16 08:33:46 by tmalidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,6 +93,10 @@ int	ft_key(int key, t_data *gd)
 	}
 	draw_floor(gd);
 	draw_player_view(gd);
+	gd->player.pos_x = gd->pos_x * (gd->max_x * gd->pixel.size) / (double)WINDOW_WIDTH;
+	gd->player.pos_y = gd->pos_y * (gd->max_y * gd->pixel.size) / (double)WINDOW_HEIGHT;
+	printf("pos y: %f pos x: %f\n", gd->player.pos_y, gd->player.pos_x);
+	show_minimap(gd);
 	mlx_put_image_to_window(gd->mlx, gd->win, gd->pixel.img, 0, 0);
 	return (0);
 }
